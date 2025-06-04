c5e56063c20aa431656e3fb105427d57458ca617
Lolo1712 <mibanezgasca@gmail.com>
2025-06-04 23:33:40 +0200

    rect colliders
    Rect Collides functions done (RectRect and CircleRect). (Not tested yet)

--- GameEngine/CCollider.cpp
+++ GameEngine/CCollider.cpp
@@ -47,7 +47,31 @@ bool CCollider::CheckRectRect(const glm::vec2& pos1, const glm::vec2& size1, con
 
 bool CCollider::CheckCircleRect(const glm::vec2& pos1, float radius1, const glm::vec2& pos2, const glm::vec2& size2)
 {
+	float testX = pos1.x;
+	float testY = pos1.y;
 
+	if (pos1.x < pos2.x) 
+	{
+		testX = pos2.x;
+	}
+	else if(pos1.x > pos2.x + size2.x)
+	{
+		testX = pos2.x + size2.x;
+	}
+
+	if (pos1.y < pos2.y)
+	{
+		testY = pos2.y;
+	}
+	else if (pos1.y > pos2.y + size2.y)
+	{
+		testY = pos2.y + size2.y;
+	}
+
+	float distance = glm::distance(pos1, glm::vec2(testX, testY));
+	if (distance <= radius1) {
+		return true;
+	}
 
 	return false;
 }
